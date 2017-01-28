746b912cc1b85a45a587af58ce083279df8c6dc7
valentin <valentin.rublack@gmail.com>
2017-01-27 20:09:41 -0500

    Using zero mean preprocessing as opposed to range preprocessing now

--- main.py
+++ main.py
@@ -5,29 +5,65 @@ from keras.metrics import mean_squared_error
 from keras.models import Sequential
 from numpy import genfromtxt
 import matplotlib.pyplot as plt
+from sklearn import preprocessing
 from sklearn.preprocessing import MinMaxScaler
 
 data = genfromtxt('data/layton_wild', delimiter=',')
 
 data = data[1:]
 
-scaler = MinMaxScaler(feature_range=(-1, 1))
-
 emg = data[::2, 1:2]
-emg = scaler.fit_transform(emg)
+# gives data zero mean
+emg = preprocessing.scale(emg)
 
 acc = data[1::2, 1:-1]
-acc = scaler.fit_transform(acc)
+# gives data zero mean
+acc = preprocessing.scale(acc)
 
 
-# plt.plot(emg)
-# plt.show()
+def plotWindow(target, predict, offset):
+    """
+    Shows plot in window
+    """ \
+        # just plot z axis for now
+    target = target[:, 2:3]
+    zPredict = predict[:, 2:3]
+
+    predictPlot = numpy.empty_like(acc)
+    predictPlot[:, :] = numpy.nan
+    predictPlot[offset:len(zPredict) + offset, :] = zPredict
+
+    # plot baseline and predictions
+    plt.plot(target, label='actual')
+    plt.plot(predictPlot, label='predict')
+
+    plt.show()
+
+
+def plotFile(target, predict, offset):
+    """
+    Writes plot to image file. Just showing a small part of the x-axis so it's big enough to see.
+    """
+
+    cutoffFrac = 0.9
+    cutoffNum = int(cutoffFrac * len(target))
+
+    # just plot z axis for now
+    target = target[cutoffNum:, 2:3]
+    zPredict = predict[cutoffNum:, 2:3]
+
+    predictPlot = numpy.empty_like(acc)
+    predictPlot[:, :] = numpy.nan
+    predictPlot[offset:len(zPredict) + offset, :] = zPredict
+
+    # plot baseline and predictions
+    plt.plot(target, label='actual')
+    plt.plot(predictPlot, label='predict')
+
+    plt.savefig('graph.png')
 
 
 def recurrent_model():
-    # Epoch 30000/30000
-    # 0s - loss: 0.6794 - acc: 0.5000
-
     features = 4
     output_dim = 3
     look_back = 25
@@ -40,12 +76,6 @@ def recurrent_model():
             X_rec[i, j, :] = numpy.append(acc[j + i], emg[j + i])
         Y_rec[i] = acc[i + look_back][:]
 
-    # train_samples = int(testcases * 0.66)
-    # X_rec_train = X_rec[:train_samples, :, :]
-    # X_rec_test = X_rec[train_samples:, :, :]
-    # Y_rec_train = Y_rec[:train_samples]
-    # Y_rec_test = Y_rec[train_samples:]
-
     model = Sequential()
     model.add(LSTM(output_dim, input_dim=features))
     # Compile model
@@ -55,30 +85,9 @@ def recurrent_model():
 
     # make predictions
     predict = model.predict(X_rec)
-    # invert predictions
-    # predict = scaler.inverse_transform(predict)
-    # Y = scaler.inverse_transform([Y_rec])
-    # predict = scaler.inverse_transform(predict)
-    # Y = scaler.inverse_transform([Y])
-    # calculate root mean squared error
-    # trainScore = numpy.sqrt(mean_squared_error(Y_rec[0], predict[:, 0]))
-    # print('Train Score: %.2f RMSE' % (trainScore))
 
-    # shift train predictions for plotting
-    target = Y_rec[:, 2:3]
-    zPredict = predict[:, 2:3]
-
-    trainPredictPlot = numpy.empty_like(acc)
-    trainPredictPlot[:, :] = numpy.nan
-    trainPredictPlot[look_back:len(zPredict) + look_back, :] = zPredict
-
-    # plot baseline and predictions
-    plt.plot(target, label='actual')
-    plt.plot(trainPredictPlot, label='predict')
-
-    plt.savefig('graph.png')
-
-    plt.show()
+    plotFile(Y_rec, predict, look_back)
+    plotWindow(Y_rec, predict, look_back)
 
 
 recurrent_model()
